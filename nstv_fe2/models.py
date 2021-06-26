59ce03f50100e7b12ab5a0e6935d3b0d236857d7
kcinnick <nicktucker4@gmail.com>
2021-06-25 23:43:53 -0400

    added IDs to models, EpisodeTestCase in tests.py & a print to views.py

--- nstv_fe2/models.py
+++ nstv_fe2/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class Show(models.Model):
-    id = models.IntegerField(primary_key=True, unique=True)
+    id = models.IntegerField(primary_key=True, unique=True, auto_created=True)
     title = models.TextField()
 
 
 class Episode(models.Model):
-    id = models.IntegerField(primary_key=True, unique=True)
+    id = models.IntegerField(primary_key=True, unique=True, auto_created=True)
     title = models.TextField()
     season = models.IntegerField()
     number = models.IntegerField()
--- nstv_fe2/views.py
+++ nstv_fe2/views.py
@@ -5,4 +5,5 @@ from .models import Show
 # Create your views here.
 def index(request):
     shows = Show.objects.all()
+    print(shows)
     return render(request, template_name='index.html')
