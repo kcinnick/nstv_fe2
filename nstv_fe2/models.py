9d1b7d81854ab8b072dad73da255d2537d2a79ff
kcinnick <nicktucker4@gmail.com>
2021-06-25 23:21:48 -0400

    added Episode & Show models & put a test query into views.py::index

--- nstv_fe2/models.py
+++ nstv_fe2/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Show(models.Model):
+    id = models.IntegerField(primary_key=True, unique=True)
+    title = models.TextField()
+
+
+class Episode(models.Model):
+    id = models.IntegerField(primary_key=True, unique=True)
+    title = models.TextField()
+    season = models.IntegerField()
+    number = models.IntegerField()
+    original_air_date = models.DateField()
+
+    show = models.ForeignKey("Show", on_delete=models.CASCADE, null=True)
--- nstv_fe2/views.py
+++ nstv_fe2/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Show
+
 
 # Create your views here.
 def index(request):
+    shows = Show.objects.all()
     return render(request, template_name='index.html')
